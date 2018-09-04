0954bf17777c37ff0249deb7f56ab3b5e5c167d2
Ubuntu <ubuntu@ip-172-31-28-70.us-west-2.compute.internal>
2018-09-04 00:58:46 +0000

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+pushd data
+wget https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Lab/train.zip
+wget https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Lab/validation.zip
+wget https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Project/sample_evaluation_data.zip
+
+popd
+
