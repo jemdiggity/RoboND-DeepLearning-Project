aaae306b45e894528670ed4213e322e6f1d1c7c6
Ubuntu <ubuntu@ip-172-31-28-70.us-west-2.compute.internal>
2018-09-04 02:11:55 +0000

    Download custom data

--- setup.sh
+++ setup.sh
@@ -4,7 +4,17 @@ wget https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Lab/
 wget https://s3-us-west-1.amazonaws.com/udacity-robotics/Deep+Learning+Data/Project/sample_evaluation_data.zip
 
 unzip train.zip
+mv train_combined train
 unzip validation.zip
 unzip sample_evaluation_data.zip
+
+mkdir -p raw_sim_data
+pushd raw_sim_data
+
+wget https://s3-us-west-2.amazonaws.com/robodn-dl/sim_data.zip
+wget https://s3-us-west-2.amazonaws.com/robodn-dl/raw_sim_data_hidden.zip
+wget https://s3-us-west-2.amazonaws.com/robodn-dl/Archive.zip
+
+popd
 popd
 
